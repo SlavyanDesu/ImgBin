7588514168696078e6ae4e81438312bb10e23e64
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2025-03-02 00:40:52 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(cors({ credentials: true }));
 app.use(session({
   secret: process.env.SESSION_SECRET!,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: { secure: true },
 }));
 
