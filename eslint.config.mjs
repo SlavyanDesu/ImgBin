298f3fde749a4beeca4c81f0bb8caf69d5d7b380
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2025-03-09 02:34:15 +0700

    fix: ejs not found

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,7 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: ["dist/*"],
+  },
 ];
